05d4aae1f82d0a1dfa08faa2a25adc8098623f19
SANielen <113352106+SANielen@users.noreply.github.com>
2025-06-03 21:18:45 +0200

    display bux fixes

--- displays.hpp
+++ displays.hpp
@@ -1,281 +1,229 @@
-#include <iostream>
+#include <cmath>
 #include <string>
-#include "displays.hpp"
+#include <vector>
 #include "hero.hpp"
 #include "enemy.hpp"
 #include "caveFactory.hpp"
-displays menu;
-hero player;
-caveFactory factory;
+#include <iostream>
+#include <chrono>       //used for sleep
+#include <thread>       //used for sleep
+#include <filesystem>   //Used for locating saves
 
-//Creating enemylist using a vectorlist. 
-std::vector<enemy> enemyList = {
-    enemy("Hest", 1, 4, 100),//                 [0]
-    enemy("Weak Goblin", 2, 4, 200),//          [1]
-    enemy("Strong Goblin", 3, 8, 400),//        [2]
-    enemy("Stronger Goblin", 4, 10, 500),//     [3]
-    enemy("Den staerkeste Goblin", 5, 15, 800),//[4]
-    enemy("Abe Kongen", 5, 30, 1000),//         [5]
-    enemy("Enhjeorning", 8, 5, 1500),//          [6]
-    enemy("Balrog", 10, 100, 3000)//             [7]
+class displays{
+
+public:
+displays(){};
+
+void bootMenuDefault(){
+    std::cout << "--------------" << std::endl;
+    std::cout << "Please enter a valid number" << std::endl;
+    std::cout << "(1) New Game" << std::endl;
+    std::cout << "(2) Load Game" << std::endl;
+    std::cout << "(0) Exit game" << std::endl;
+    std::cout << "--------------" << std::endl;
 };
 
-int selection;
-bool running = true;
+void bootMenu(){
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "(1) New Game" << std::endl;
+    std::cout << "(2) Load Game" << std::endl;
+    std::cout << "(0) Exit game" << std::endl;
+    std::cout << "--------------" << std::endl;
+}
 
-
-bool load(){
+void loadSuccessful() {
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "LOAD SUCCESSFUL" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "--------------" << std::endl;
     
-    while (running){
-    if (menu.loadMenu()){
-        std::string loadName;
-        std::cin >> loadName;
-            if(!player.loadHero(loadName)){
-                menu.loadFailedMenu();
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-            } else {
-                menu.loadSuccessful();
-                player.resetHeroHealth();
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                return true;
+}
+
+void loadFailedMenu() {
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "LOAD FAILED" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "--------------" << std::endl;
+    
+}
+
+void loadFailedMenu2() {
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "NO SAVE FOLDER FOUND" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "--------------" << std::endl;
+
+}
+
+bool loadMenu() {
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "Type in the name of the game file you want to load:" << std::endl;
+
+    std::string folder = "saves";
+    int count = 0;
+
+    if (!std::filesystem::exists(folder)) {
+        loadFailedMenu2();
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        return false;
+    } else {
+        std::vector<std::filesystem::directory_entry> entries;
+
+        // get saves
+        for (std::filesystem::directory_iterator it(folder); it != std::filesystem::directory_iterator(); ++it) {
+            if (it->path().extension() == ".txt") {
+                entries.push_back(*it);
             }
-        } else {
+        }
+        //show saves
+        for (size_t i = 0; i < entries.size(); ++i) {
+            std::cout << "- " << entries[i].path().stem().string() << std::endl;
+            count++;
+        }
+
+        if (count == 0) {
+            loadFailedMenu();
+            std::this_thread::sleep_for(std::chrono::seconds(1));
             return false;
         }
-    };
+    }
+
+    std::cout << "--------------" << std::endl;
     return true;
 }
-    
-void pause(){
-    bool pauseRun = true;
-    while (pauseRun){
-    menu.pauseMenu();    
-    std::cin >> selection;
-        switch (selection){
-            case 1:                     //Resume
-            {
-                return;
-            }
-            case 2:                     //Save Game
-            {
-                if (!player.saveHero()){
-                    menu.saveFailedScreen();
-                    std::this_thread::sleep_for(std::chrono::seconds(1));
-                    continue;
-                } else {
-                    menu.saveScreen();
-                    std::this_thread::sleep_for(std::chrono::seconds(1));
-                    continue;
-                }
-            } 
-            case 3:                     //Load Game
-            {
-                if (load()){
-                    pauseRun = false;
-                }
-                continue;
-            }              
-            case 0:                     //Exit the game
-            {
-                exit(0);
-            }
-        }
-    }
-    return;
+
+void startMenu(hero player){
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "Enter your Hero's name" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "--------------" << std::endl;
+
 };
 
-void postFight(bool battleWon, hero& player, enemy& foe) {
-    if (battleWon) {
-        player.addXP(foe.getEnemyXP());
-        player.checkLevelUp();
-        menu.battleWon(foe);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    } else {
-        menu.battleLost(foe);
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
-}
+void mainMenu(hero player){
+    std::cout << "--------------" << std::endl;
+    std::cout << player.getHeroName() << " has: "
+          << player.getXP() << " XP | "
+          << "Gold: " << player.getGold() << " | "
+          << "Level: " << player.getLvl() << std::endl;
+    std::cout << " current weapon: " << player.getWeapon().getName()
+    << " (Durability: " << player.getWeapon().getDurability() << ")" << std::endl;
+    std::cout << "(1) Fight monsters" << std::endl;
+    std::cout << "(2) Enter Cave" << std::endl;
+    std::cout << "(3) Show statistics" << std::endl;
+    std::cout << "(0) Pause Menu" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "--------------" << std::endl;
+};
 
-
-bool battle(hero& player, enemy foe) {
-    bool battleRun = true;
-    bool battleWon = false;
-
-    while (battleRun) {
-        menu.battleMenu(player, foe);
-        std::cin >> selection;
-
-        switch (selection) {
-            case 1: // Attack
-                foe.takeDamage(player.getHeroDmg());
-                if (foe.getEnemyHP() <= 0) {
-                    battleWon = true;
-                    battleRun = false;
-                    continue;
-                }
-
-                player.takeDamage(foe.getEnemyDmg());
-                if (player.getHeroHP() <= 0) {
-                    battleWon = false;
-                    battleRun = false;
-                }
-                break;
-
-            case 2: // Flee
-                battleRun = false;
-                break;
-
-            case 3: // Do nothing (debug)
-                player.takeDamage(foe.getEnemyDmg());
-                if (player.getHeroHP() <= 0) {
-                    battleWon = false;
-                    battleRun = false;
-                }
-                break;
-
-            case 0: // Pause
-                pause();
-                break;
-
-            default:
-                break;
-        }
-    }
-
-    postFight(battleWon, player, foe);
-    return battleWon;
-}
-
-
-void fightMonsters() {
-    bool fightRun = true;
-    while (fightRun) {
-        menu.fightMonsters(enemyList);
-        std::cin >> selection;
-        if (selection == 0) {
-            return;
-        }
-
-        if (selection > 0 && selection < enemyList.size()) {
-            //Calling a copy constructor during initialization
-            enemy chosenEnemy = enemyList[selection - 1];
-
-            battle(player, chosenEnemy);
-            fightRun = false;
-        } else {
-            continue;
-        }
-    }
-    player.resetHeroHealth();
-}
-
-void enterCave() {
-    std::vector<Cave> caves;
-
-    for (int i = 0; i < 8; ++i) {
-        caves.push_back(factory.createCave(player, enemyList, i));
-    }
-
-    bool enterCaveRun = true;
-    while (enterCaveRun) {
-        menu.showCaves(caves);
-        std::cin >> selection;
-
-        if (selection == 0) return;
-
-        if (selection > 0 && selection <= caves.size()) {
-            Cave chosen = caves[selection - 1];
-            bool cleared = true;
-
-            for (int i = 0; i < chosen.enemies.size(); ++i) {
-                if (!battle(player, chosen.enemies[i]) || player.getHeroHP() <= 0) {
-                    cleared = false;
-                    break;
-                }
-            }
-
-            if (cleared) {
-                player.addGold(chosen.goldReward);
-                menu.caveWon(chosen);
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-            }
-
-            enterCaveRun = false;
-        }
-    }
-    player.resetHeroHealth();
-}
-
-
-int main(int argc, char *argv[])
-{
-    
-    //Startup of the game
-    while (running)
+void fightMonsters(std::vector<enemy> list){
+    std::cout << "--------------" << std::endl;
+    std::cout << "Choose your opponent" <<  std::endl;
+    for (int i = 0; i < list.size(); i++)
     {
-        menu.bootMenu();
-        std::cin >> selection;
-
-        switch (selection)
-        {
-            case 1:
-            {
-                menu.startMenu(player);
-                std::string nameHolder;
-                std::cin >> nameHolder;
-                player.setName(nameHolder);
-                running = false;
-                break;
-            }
-            case 2:
-            {
-                if (load()){
-                    running = false;
-                }
-                break;
-            } 
-            case 0:
-            {
-                exit (0);
-            }
-
-            default:
-                menu.bootMenuDefault();
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-        };
+        std::cout << "(" << i + 1 << ") " << list[i].getEnemyName() <<  std::endl;
+        std::cout << "   DMG:" << list[i].getEnemyDmg() 
+                  << " Health: " << list[i].getEnemyHP() 
+                  << " XP: " << list[i].getEnemyXP() << std::endl;
     }
-    
+    std::cout << " " << std::endl;
+    std::cout << "(0) nevermind. I'm a chicken" << std::endl;
+    std::cout << "--------------" << std::endl;
+};
 
-    //Playing game (mainMenu)
-    running = true;
-    while (running)
-    {
-    menu.mainMenu(player);
-    std::cin >> selection;
-        switch (selection)
-        {
-            case 1:
-            {
-                fightMonsters();
-                continue;
-            }
-            case 2:
-            {
-                enterCave();
-                continue;
-            }
-            case 0:
-            {
-                pause();
-            }
+void battleMenu(hero player, enemy foe){
+    std::cout << "--------------" << std::endl;
+    std::cout << player.getHeroName() << " HP: " << player.getHeroHP() << " | " <<  std::endl;
+    std::cout << foe.getEnemyName() << " HP: " << foe.getEnemyHP() << " | " <<  std::endl;
+    std::cout << "(1) Attack!" << std::endl;
+    std::cout << "(2) Flee" << std::endl;
+    std::cout << "(0) Pause" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "--------------" << std::endl;
+};
 
-            default:
-                continue;
-        }
+void pauseMenu(){
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "(1) Resume game" << std::endl;
+    std::cout << "(2) Save game" << std::endl;
+    std::cout << "(3) Load game" << std::endl;
+    std::cout << "(0) Exit Game" << std::endl;
+    std::cout << "--------------" << std::endl;
+};
+
+void saveScreen(){
+    std::cout << "--------------" << std::endl;
+    std::cout << " GAME SAVED" << std::endl;
+    std::cout << "--------------" << std::endl;
+};
+void saveFailedScreen(){
+    std::cout << "--------------" << std::endl;
+    std::cout << " GAME FAILED TO SAVE" << std::endl;
+    std::cout << "--------------" << std::endl;
+};
+
+void battleWon(enemy foe){
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "    VICTORY   " << std::endl;
+    std::cout << "You have slain a " << foe.getEnemyName() 
+    << " Earning: " << foe.getEnemyXP() << " XP" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "--------------" << std::endl;
+};
+void caveWon1(Cave cave){
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "    CONGRATS   " << std::endl;
+    std::cout << "You cleared the cave! " << cave.name << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "--------------" << std::endl;
+};
+void caveWon2(Cave cave, weapon foundWeapon){
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "    CONGRATS   " << std::endl;
+    std::cout << "You cleared the cave! " << cave.name 
+    << " Earning: " << cave.goldReward << " Gold" << std::endl;
+    std::cout << "You have found a: " << foundWeapon.getName() << std::endl;
+    std::cout << "--------------" << std::endl;
+};
+void battleLost(enemy foe){
+    std::cout << "--------------" << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "womp womp, you lost to a " << foe.getEnemyName() << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << " " << std::endl;
+    std::cout << "--------------" << std::endl;
+};
+
+void showCaves(std::vector<Cave>& caves) {
+    std::cout << "--------------" << std::endl;
+    std::cout << "Available Caves:" << std::endl;
+    for (size_t i = 0; i < caves.size(); ++i) {
+        std::cout << "(" << i + 1 << ") " << caves[i].name
+                  << " | Enemies: " << caves[i].enemies.size()
+                  << " | Gold Reward: " << caves[i].goldReward << std::endl;
     }
-    
-    
-
-    return 0;
+    std::cout << "(0) You know what. Naaah" << std::endl;
+    std::cout << "--------------" << std::endl;
 }
+
+
+private:
+
+
+
+};
 
